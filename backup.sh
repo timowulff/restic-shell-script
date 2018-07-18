506d1aca2f4e07aeefab04058344cfbd21dd246c
zeik <zeik@edr35812.dip.tu-dresden.de>
2018-07-18 17:37:48 +0200

    update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,14 +10,14 @@ backupdir=/Volumes/${backupvolume}/backup-restic
 export RESTIC_PASSWORD=$(security 2>&1 >/dev/null find-generic-password -ga ${resticuser} |ruby -e 'print $1 if STDIN.gets =~ /^password: "(.*)"$/')
 
 # Backups
-restic -r ${backupdir} backup /Users/timowulff/
-#restic -r ${backupdir} backup /Library/
+restic -r ${backupdir} backup /Users/$USER/ --exclude ~/Documents/SHK
 restic -r ${backupdir} backup /Applications/
+restic -r ${backupdir} backup /Library/Mail/
 restic -r ${backupdir} snapshots
 
 # Unmount $(backupvolume)
-diskutil unmount /dev/$(diskutil list | grep ${backupvolume} | awk '{print $6}')
+# diskutil unmount /dev/$(diskutil list | grep ${backupvolume} | awk '{print $6}')
 
 # Message
 osascript -e beep
-osascript -e 'tell app "System Events" to display dialog "Backup fertig - Festplatte unmounted!" buttons {"OK"} default button 1 with icon caution with title "Backup Toschiba"'
+osascript -e 'tell app "System Events" to display dialog "Backup done - don't forget to unmount!" buttons {"OK"} default button 1 with icon caution with title "Backup Toschiba ohne Sticker"'
